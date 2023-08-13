94a31ec7cbd58179896bf2778018ca9c177ccf7f
Ryan White <ryanwhite_1@yahoo.com.au>
2023-08-13 15:22:12 +1000

    replot with no spin

--- Question2.py
+++ Question2.py
@@ -267,32 +267,32 @@ def root_find(xarr, mass):
 
 ### Q4b ###
 
-# x = np.logspace(0, 3, 100)  # initialise temps
+x = np.logspace(0, 3, 100)  # initialise temps
 
-# fig, ax = plt.subplots(figsize=(4.5, 5))    # make figure
+fig, ax = plt.subplots(figsize=(4.5, 5))    # make figure
 
-# # define parameters we want to look at
-# masses = [40, 60, 80, 100, 100, 200]
-# lambdas = [1e-3, 1e-3, 1e-3, 1e-3, 1e-4, 1e-3]
+# define parameters we want to look at
+masses = [40, 60, 80, 100, 100, 200]
+lambdas = [1e-3, 1e-3, 1e-3, 1e-3, 1e-4, 1e-3]
 
-# for i, mass in enumerate(masses):
-#     y = dimenless_abund(x, mass, 1/2, [], [True, lambdas[i]])     # solve for the abundance across our temps
-#     ax.plot(x, y, label=f'$m_S$={mass}, $\lambda={lambdas[i]}$')   # and plot it on the figure
+for i, mass in enumerate(masses):
+    y = dimenless_abund(x, mass, 0, [], [True, lambdas[i]])     # solve for the abundance across our temps
+    ax.plot(x, y, label=f'$m_S$={mass}, $\lambda={lambdas[i]}$')   # and plot it on the figure
     
-# ax.set_xscale('log'); ax.set_yscale('log')
-# ax.set_xlabel("$x = m / T$"); ax.set_ylabel("Abundance $Y$")
-# ax.legend(loc='upper right')
-# ax.set_ylim(ymax=0.1)   # set a bit higher so that the legend fits nicely
+ax.set_xscale('log'); ax.set_yscale('log')
+ax.set_xlabel("$x = m / T$"); ax.set_ylabel("Abundance $Y$")
+ax.legend(loc='upper right')
+ax.set_ylim(ymax=0.1)   # set a bit higher so that the legend fits nicely
 
-# fig.savefig("Q4b.png", dpi=400, bbox_inches='tight')    # save figures as png (to look at) and pdf (for the report)
-# fig.savefig("Q4b.pdf", dpi=400, bbox_inches='tight')
+fig.savefig("Q4b.png", dpi=400, bbox_inches='tight')    # save figures as png (to look at) and pdf (for the report)
+fig.savefig("Q4b.pdf", dpi=400, bbox_inches='tight')
 
 
 
 ### Q4c ###
 x = np.logspace(0, 3, 20)
 # masses = np.logspace(np.log10(50), 3, 100) # big range of masses to look at
-masses = np.linspace(50, 200, 100)
+masses = np.linspace(50, 200, 150)
 # ovs = np.linspace(-30, -26, 5) # look at a few cross sections - this is a linspace, but these values will be used as powers
 
 # ys = np.zeros((len(masses), len(ovs))) # initialise array of data
@@ -301,7 +301,7 @@ ys = np.zeros(len(masses))
 for i, mass in enumerate(masses):
     # ys[i] = cosmo_abund([], x, mass, 1/2, 0, [True, 1e-3]) # calculate present day val for this mass and cross section, and store it
     # ys[i] = dimenless_abund(x, mass, 1/2, [], [True, 1e-3])[-1]
-    arr = dimenless_abund(x, mass, 1/2, [], [True, 1e-3])
+    arr = dimenless_abund(x, mass, 0, [], [True, 1e-3])
     index = len(masses) - 1
     for j, val in enumerate(arr):
         if np.isnan(val):
